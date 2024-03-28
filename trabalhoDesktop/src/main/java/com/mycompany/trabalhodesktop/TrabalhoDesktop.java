ca75966b6eed404fd049751537cdd57ebfb4d15e
Vinicius Sempkoski <98129502+vinicius-sempkoski@users.noreply.github.com>
2024-03-27 22:14:48 -0300

    Add files via upload

---
+++ trabalhoDesktop/src/main/java/com/mycompany/trabalhodesktop/TrabalhoDesktop.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhodesktop;
+
+/**
+ *
+ * @author aluno
+ */
+public class TrabalhoDesktop {
+
+    public static void main(String[] args) {
+        new telaLogin().setVisible(true);
+        
+    }
+}
